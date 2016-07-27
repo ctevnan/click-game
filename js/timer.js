93fea8c5f69e530c259e94ed0548598c8f671995
ctevnan <carolyntevnan@yahoo.com>
2016-07-27 11:29:47 -0400

    adding timer. doing this quickly because the other version of this project didnt deploy correctly

---
+++ js/timer.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  var trackGameScore = 0;
+  
+  $("td img").addClass("img-responsive");
+
+  $(".img-responsive").attr("disabled", true);
+
+  function countInc() {
+    trackGameScore +=1;
+
+    $(".score").text(trackGameScore);
+  }
+
+
+  function countDescd() {
+    setTimeout(function count() {
+      $("#scoreModal .modal-body").empty();
+
+      $("#scoreModal .modal-body").append("<p>The game is over. You clicked " + trackGameScore + " eggs!</p>");
+
+      $("#scoreModal").modal("show");
+
+      $("#scoreModal").on("hide.bs.modal", function() {
+        $("score").text("");
+      });
+      $(".img-responsive").attr("disabled", true);
+    }, 20000);
+  }
+
+  $(".img-responsive").on("click", function() {
+    if ($(this).attr("disabled")) {
+      return;
+    }
+    $(this).removeClass("gameRunning").addClass("gameNotRunning").attr("disabled", true);
+    countInc();
+  });
+  
+  $(".btn-block").on("click", function() {
+    trackGameScore =0;
+
+    $(".img-responsive").attr("disabled", false).removeClass("gameNotRunning").addClass("gameRunning");
+
+    countDescd();
+  });  
+});
